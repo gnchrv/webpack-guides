414c47ea949ce8c8693b0f915ec99832e19cb3ff
Misha Goncharov <forsomeletters@gmail.com>
2019-11-16 17:42:29 +0300

    Add CSS loaders

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import _ from 'lodash'
+import './style.css'
 
 function component() {
   const el = document.createElement('div')
+
   el.innerHTML = ['Hello', 'webpack'].join(' ')
+  el.classList.add('hello')
+
   return el
 }
 
