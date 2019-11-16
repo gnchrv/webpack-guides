e3b7ae25db5dfddeb86ec9d7f7fc89e296eaeef9
Misha Goncharov <forsomeletters@gmail.com>
2019-11-16 17:59:03 +0300

    Add image loading

--- src/index.js
+++ src/index.js
@@ -1,11 +1,20 @@
 import _ from 'lodash'
 import './style.css'
+import Logo from './webpack-logo.svg'
 
 function component() {
+  // Create a div element
   const el = document.createElement('div')
 
+  // Add styling and some text
   el.innerHTML = ['Hello', 'webpack'].join(' ')
   el.classList.add('hello')
+
+  // Load the image and add it to the created element
+  const img = new Image()
+  img.src = Logo
+  img.style.height = '1em';
+  el.appendChild(img)
 
   return el
 }
