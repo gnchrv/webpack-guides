cb7b9524874bca6f0ab8de05c48e4bc59533fb7d
Misha Goncharov <forsomeletters@gmail.com>
2019-11-19 20:28:35 +0300

    Setting up HtmlWebpackPlugin

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
 import _ from 'lodash'
+import printMe from './print'
 
 function component() {
   // Create a div element
   const el = document.createElement('div')
+  const btn = document.createElement('button')
 
   // Add styling and some text
   el.innerHTML = ['Hello', 'webpack'].join(' ')
+  btn.innerHTML = 'Click me and check the console!'
+  btn.onclick = printMe
+
+  el.appendChild(btn)
 
   // Return the element
   return el
