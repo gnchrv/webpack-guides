a3c4e9d20c94a0807847662a2087131deb5fdee5
Misha Goncharov <forsomeletters@gmail.com>
2019-11-28 01:35:18 +0300

    Code Splitting: Entry Points

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,7 @@ module.exports = {
   },
   entry: {
     app: './src/index.js',
-    print: './src/print.js'
+    another: './src/another-module.js'
   },
   output: {
     filename: '[name].bundle.js',
